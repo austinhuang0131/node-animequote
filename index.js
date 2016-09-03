6a6e77ae1fc1df9e6a3d62f25f90d95ed91925b9
Austin Huang <austinhuang0131@gmail.com>
2016-09-02 21:46:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -120,329 +120,282 @@ var animequote = [
     "quotecharacter": "- Fuyumi Irisu,",
     "quoteanime": " Hyouka"
   },
-  
   {
     "quotenumber": 21,
     "quotesentence": "Even if he hates me, being able to die with the knowledge that the one I love will never forget me… there is no greater happiness a woman could desire.",
     "quotecharacter": "- Hakaze Kusaribe,",
     "quoteanime": " Zetsuen no Tempest"
   },
-  
   {
     "quotenumber": 22,
     "quotesentence": "I wasn’t scared that I’d be rejected, I was scared that if I said how I felt, all of our relationships would change.",
     "quotecharacter": "- Chisaki Hiradaira,",
     "quoteanime": " Nagi no Asukara"
   },
-  
   {
     "quotenumber": 23,
     "quotesentence": "Do you know what's the most important thing about climbing mountains is? It's not about having the courage to challenge it, It's about having the courage to back down.",
     "quotecharacter": "- Nozomi Tōjō,",
     "quoteanime": " Love Live! School idol project"
   },
-  
   {
     "quotenumber": 24,
     "quotesentence": "You may be unfortunate, but that doesn't mean you have to suffer. You may not be blessed, but that doesn't mean you have to throw a fit over it. Even if bad things happen to you, just be strong!",
     "quotecharacter": "- Koyomi Araragi,",
     "quoteanime": " Bakemonogatari"
   },
-  
   {
     "quotenumber": 25,
     "quotesentence": "A faint clap of thunder, clouded skies; perhaps rain comes. If so, will you stay here with me?",
     "quotecharacter": "- Yukari Yukino,",
     "quoteanime": " Kotonoha no Niwa"
   },
-  
   {
     "quotenumber": 26,
     "quotesentence": "Hold on. Don't go! Let's argue again. I'll bribe you with a canelé. I'll call you to kill time. I don't mind being Friend A. Please don't go. Please don't go. Please don't go...please don't leave me behind.",
     "quotecharacter": "- Kousei Arima,",
     "quoteanime": " Shigatsu wa Kimi no Uso"
   },
-  
   {
     "quotenumber": 27,
     "quotesentence": "Having happy and beautiful memories won't always bring you salvation. The more beautiful a memory is, the more painful it can become. It can even become terrifying. Both for the one who's leaving... And for the one left behind.",
     "quotecharacter": "- Isla,",
     "quoteanime": " Plastic Memories"
   },
-  
   {
     "quotenumber": 28,
     "quotesentence": "Even if I know you'll reject me, even if I'm afraid you won't accept me, even if I know it will really hurt if you dump me - I still have to tell you how I feel!",
     "quotecharacter": "- Kousaka Kyousuke ,",
     "quoteanime": " Ore no Imouto ga Konnani Kawaii Wake ga Nai"
   },
-  
   {
     "quotenumber": 29,
     "quotesentence": "There is nothing that you can acquire without making a move on your own. It's the same with romance - your only hope is to be more aggressive.",
     "quotecharacter": "- Marika Tachibana,",
     "quoteanime": " Nisekoi"
   },
-  
   {
     "quotenumber": 30,
     "quotesentence": "It makes you sad when someone suddenly just up and disappears without a word. If you're just going to end up hurt in the end, you're better off not forming any bonds at all.",
     "quotecharacter": "- Yozora Mikazuki,",
     "quoteanime": " Boku wa Tomodachi ga Sukunai"
   },
-  
   {
     "quotenumber": 31,
     "quotesentence": "Well, it's an anime, so you shouldn't think so hard about it.",
     "quotecharacter": "- Illyasviel von Einzbern,",
     "quoteanime": " Fate/kaleid liner Prisma☆Illya "
   },
-  
   {
     "quotenumber": 32,
     "quotesentence": "You were ready to kill another creature, yet you’re scared of being killed yourself. Don’t you think that is strange? When you point a gun at a living thing… this is what that act means. ",
     "quotecharacter": "- Kurumi Tokisaki,",
     "quoteanime": " Date A Live"
   },
-  
   {
     "quotenumber": 33,
     "quotesentence": "Watching TV or playing games alone is more fun. Playing along with your friends' hobbies... Laughing at their jokes when they're not funny... All that does is tire me out.",
     "quotecharacter": "- Yuzuru Otonashi,",
     "quoteanime": " Angel Beats!"
   },
-  
   {
     "quotenumber": 34,
     "quotesentence": "There’s no hope on the battlefield. It has nothing but unspeakable despair. Just a crime we call victory, paid for by the pain of the defeated. Yet humanity has never recognized this truth. And the reason for that is, in every era, a dazzling hero has blinded people with their legends and prevented them from seeing the evil of bloodshed. The true nature of humanity has not advanced a step beyond the Stone Age.",
     "quotecharacter": "- Emiya Kiritsugu,",
     "quoteanime": " Fate/Zero"
   },
-  
   {
     "quotenumber": 35,
     "quotesentence": "Wishes are great because they don’t come true. We work hard because they don’t. We constantly push ourselves forward precisely because they don’t come true. But once they do what’s left to keep driving us forward?",
     "quotecharacter": "- Alis Color,",
     "quoteanime": " Kamisama no Inai Nichiyoubi"
   },
-  
   {
     "quotenumber": 36,
     "quotesentence": "It is a poor knight indeed who slays one who defies him. The key is to show yourself to be superior that they will not defy you in the first place.",
     "quotecharacter": "- Slaine Troyard,",
     "quoteanime": " Aldnoah.Zero"
   },
-  
   {
     "quotenumber": 37,
     "quotesentence": "I always have this feeling... The feeling that I'm losing something. The world is really beautiful, but... it feels like it's just me... who's drifting away from it.",
     "quotecharacter": "- Sayuri Sawatari,",
     "quoteanime": " Kumo no Mukou, Yakusoku no Basho"
   },
-  
   {
     "quotenumber": 38,
     "quotesentence": "The moment you say a word of parting, you've already parted. So long as you and I are both somewhere in this world, we haven’t parted. So long as you don’t say it, you haven’t parted.",
     "quotecharacter": "- Satone Shichimiya,",
     "quoteanime": " Chuunibyou Demo Koi Ga Shitai!"
   },
-  
   {
     "quotenumber": 39,
     "quotesentence": "Because she learned about happiness, she feels lonely for the first time. And because she knows unhappiness, she understands happiness for the first time.",
     "quotecharacter": "- Himura Yuu,",
     "quoteanime": " Ef - A Fairy Tale Of The Two"
   },
-  
   {
     "quotenumber": 40,
     "quotesentence": "Playing a game by yourself is boring. You need a loser to crush so badly they can't recover.",
     "quotecharacter": "- Five,",
     "quoteanime": " Zankyou no Terror"
   },
-  
   {
     "quotenumber": 41,
     "quotesentence": "It's okay. Fuck my brains out.",
     "quotecharacter": "- Sora Kasugano,",
     "quoteanime": " Yosuga no Sora: In Solitude, Where We Are Least Alone."
   },
-  
   {
     "quotenumber": 42,
     "quotesentence": "Love, passion, why do we get caught up by such troublesome feelings? The mind couldn't ever get things straight, and you lose control to know what is sensible. Deep down it's all so vexing.",
     "quotecharacter": "- Takumi Usui,",
     "quoteanime": " Kaichou Wa Maid-Sama!"
   },
-  
   {
     "quotenumber": 43,
     "quotesentence": "We finally became friends. I've said everything I wanted to say. Now we know everything about each other. Though, from the moment we become friends, we'll never speak to each other again.",
     "quotecharacter": "- Touma Kazusa,",
     "quoteanime": " White Album 2"
   },
-  
   {
     "quotenumber": 44,
     "quotesentence": "People don't realize they aren't able to choose whether or not to believe something. If they subconsciously believe something, therein lies the potential for a curse.",
     "quotecharacter": "- Kanoe Yuuko,",
     "quoteanime": " Tasogare Otome X Amnesia"
   },
-  
   {
     "quotenumber": 45,
     "quotesentence": "Accident or amnesia, you can't rewind your life and start again. If you struggle, you'll just hurt yourself, and the people around you.",
     "quotecharacter": "- Tada Banri,",
     "quoteanime": " Golden Time"
   },
-  
   {
     "quotenumber": 46,
     "quotesentence": "Onii-chan! You're going to split me in half!",
     "quotecharacter": "- Every Imouto,",
     "quoteanime": " in Every Incest Hentai Ever"
   },
-  
   {
     "quotenumber": 47,
     "quotesentence": "Fake people have an image to maintain. Real people just don't care.",
     "quotecharacter": "- Hachiman Hikigaya,",
     "quoteanime": " Yahari Ore No Seishun Love Come Wa Machigatteiru"
   },
-  
   {
     "quotenumber": 48,
     "quotesentence": "Panties, Panties, Panties!",
     "quotecharacter": "- Chii,",
     "quoteanime": " Chobits"
   },
-  
   {
     "quotenumber": 49,
     "quotesentence": "Brilliant people shine more brightly than ordinary ones. They never regret or agonize over what they've done.",
     "quotecharacter": "- Archer,",
     "quoteanime": " Fate/stay night: Unlimited Blade Worlks"
   },
-  
   {
     "quotenumber": 50,
     "quotesentence": "A suicide means you've killed the most important existence of all: yourself.",
     "quotecharacter": "- Monokuma,",
     "quoteanime": " Danganronpa"
   },
-  
   {
     "quotenumber": 51,
     "quotesentence": "Skipping school for one day might not be a problem, but if you skip for three days in a row, it'd be really hard to convince yourself to go back.",
     "quotecharacter": "- Ushiromiya Ange,",
     "quoteanime": " Umineko no Naku Koro ni"
   },
-  
   {
     "quotenumber": 52,
     "quotesentence": "People who don't work hard don't have the right to be envious of the people with talent. People fail because they don't understand the hard work necessary to be successful.",
     "quotecharacter": "- Yukino Yukinoshita,",
     "quoteanime": " Yahari Ore no Seishun Love Come wa Machigatteiru."
   },
-  
   {
     "quotenumber": 53,
     "quotesentence": "In our society, letting others find out that you're a nice person is a very risky move. It's extremely likely that someone would take advantage of that.",
     "quotecharacter": "- Hitagi Senjougahara,",
     "quoteanime": " Bakemonogatari"
   },
-  
   {
     "quotenumber": 54,
     "quotesentence": "If everyone were perfect, there would be no need to look out for others. Sympathy or love is needed because people are imperfect. A perfect human cannot love anyone.",
     "quotecharacter": "- Keima Katsuragi,",
     "quoteanime": " Kami Nomi zo Shiru Sekai"
   },
-  
   {
     "quotenumber": 55,
     "quotesentence": "I am who I am now because of everything that's happened. If I try to deny my past, I'm denying the person I've become. I don't want to do that.",
     "quotecharacter": "- Iori Nagase,",
     "quoteanime": " Kokoro Connect"
   },
-  
   {
     "quotenumber": 56,
     "quotesentence": "If only I could stand on a larger stage and have even more fans…but I realized something after yesterday’s performance. I was naive. Ultimately, what I enjoyed was being coddled by the fans of this familiar store.",
     "quotecharacter": "- Miyu Okamoto,",
     "quoteanime": " Wake Up, Girls!"
   },
-  
   {
     "quotenumber": 57,
     "quotesentence": "I think there are many things in this world we are better off not knowing. Sometimes the truth is the cruelest thing of all. Not everyone can bear it. ",
     "quotecharacter": "- Maria Akitsuki,",
     "quoteanime": " Shinsekai Yori"
   },
-  
   {
     "quotenumber": 58,
     "quotesentence": "Death is terrible for anyone. Young or old, good or evil, it's all the same. Death is impartial. There is no especially terrible death. That's why death is so fearsome. Your deeds, your age, your personality, your wealth, your beauty... They are all meaningless in the face of death.",
     "quotecharacter": "- Sunako Kirishiki,",
     "quoteanime": " Shiki"
   },
-  
   {
     "quotenumber": 59,
     "quotesentence": "Keeping all the wealth for yourself may make your rich, but it won’t make you prosperous. You give people money, and they spend it. True prosperity comes from a free flow of wealth and goods.",
     "quotecharacter": "- Maou,",
     "quoteanime": " Maoyuu Maou Yuusha"
   },
-  
   {
     "quotenumber": 60,
     "quotesentence": "People aren't complicated. We work hard when we know that we'll get something in return, and we try to work even harder if that something is our own improvement.",
     "quotecharacter": "- Kumiko Oumae,",
     "quoteanime": " Hibike! Euphonium"
   },
-  
   {
     "quotenumber": 61,
     "quotesentence": "No person can become a replacement for another. Not even the past version of themselves.",
     "quotecharacter": "- Ushiromiya Ange,",
     "quoteanime": " Umineko no Naku Koro ni"
   },
-  
   {
     "quotenumber": 62,
     "quotesentence": "If you claim to love humans, that should include their lies and betrayals, their sins and ugliness, their jealousy and immaturity, their hatred and conflicts.",
     "quotecharacter": "- Myouri Unzen,",
     "quoteanime": " Medaka Box"
   },
-  
   {
     "quotenumber": 63,
     "quotesentence": "Where will we be in five or ten years from now? No one knows. All we know right now, all we can see right now, is what's right in front of us. That's why we'll always live our 'right now' to the fullest!",
     "quotecharacter": "- Kanzaki Tomoyo,",
     "quoteanime": " Inou Battle wa Nichijou-kei no Naka de"
   },
-  
   {
     "quotenumber": 64,
     "quotesentence": "Don't believe in the you that believes in me and don't believe in the me that believes in you. Believe in the you that believes in yourself!",
     "quotecharacter": "- Kamina,",
     "quoteanime": " Tengen Toppa Gurren Lagann"
   },
-  
   {
     "quotenumber": 65,
     "quotesentence": "Life eventually ends. But that doesn't mean that the fact that we met, or the time we spent together will disappear.",
     "quotecharacter": "- Mei,",
     "quoteanime": " Arashi no Yoru ni"
   },
-  
   {
     "quotenumber": 66,
     "quotesentence": "I have someone I like. I never had the confidence and I’ve doubted my feelings countless of times. But, when she stays by my side and smiles with me, I feel like I can do anything.",
     "quotecharacter": "- Yoshida Haru,",
     "quoteanime": " Tonari No Kaibutsu-kun"
   },
-  
   {
     "quotenumber": 67,
     "quotesentence": "If you look away and just turn your back on those you don't understand, you'll regret it someday. Accept what's happening before your eyes as a fact. That's a shortcut to becoming an adult.",
@@ -473,7 +426,6 @@ var animequote = [
     "quotecharacter": "- Kurokami no Onna,",
     "quoteanime": " Death Parade"
   },
-  
   {
     "quotenumber": 72,
     "quotesentence": "Greed may not be good, but it’s not so bad either. You humans think greed is just for money or power, but everyone wants something they can’t have.",
@@ -532,4 +484,4 @@ var animequote = [
 
 module.exports = function() {
   return animequote[Math.floor(Math.random() * animequote.length)];
-},
+}
