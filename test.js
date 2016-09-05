633db8c92ce0789d231a7311f93139e4d72c41aa
Austin Huang <austinhuang0131@gmail.com>
2016-09-05 14:55:11 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-'use strict';
-var assert = require('assert');
-var animequote = require('./index');
 
-
-it('should get return at least one quote', function (cb) {
-		assert(animequote);
-		cb();
-});
